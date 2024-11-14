5c1384c9a20844f252823040e22f2d074197d5b5
ALvaro <aruiz58967@gmail.com>
2024-11-13 21:09:41 -0600

    creacion de tabla de eventos y la tabla de roles

---
+++ migrations/20241114001613-create-event.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      eventTypeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'EventTypes',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  },
+};
