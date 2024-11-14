2985d1107499aad141371e74aa19cdfe44aadace
ALvaro <aruiz58967@gmail.com>
2024-11-13 21:16:22 -0600

    configuracion de swagger

---
+++ swaggerConfig.js
@@ -0,0 +1,27 @@
+
+// swaggerConfig.js
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API de Eventos Corporativos',
+    version: '1.0.0',
+    description: 'Documentación de API para gestionar eventos, tipos de eventos y asistentes',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000/api',
+      description: 'Servidor de desarrollo',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'], // Ruta donde están definidas las rutas con comentarios Swagger
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
